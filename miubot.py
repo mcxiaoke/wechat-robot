362cb17e267b4c2c796a20fecd1f4af27108c131
mcxiaoke <mcxiaoke@live.com>
2018-01-30 09:10:37 -0500

    add miu bot

--- miubot.py
+++ miubot.py
@@ -9,22 +9,22 @@ import requests
 from werobot import WeRoBot
 from werobot.replies import TextReply, ImageReply, SuccessReply
 
-from config import WECHAT_TOKEN, WECHAT_AES_KEY, WECHAT_APPID, WECHAT_APPSECRET
+from config import WECHAT2_TOKEN, WECHAT2_AES_KEY, WECHAT2_APPID, WECHAT2_APPSECRET
 from const import WECHAT_CAT_WORDS, WECHAT_ERROR_HTML
-from utils import get_media_ids, get_poem_lines
+from utils import get_media_ids2, get_poem_lines2
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-robot = WeRoBot()
-robot.config['TOKEN'] = WECHAT_TOKEN
-robot.config['ENCODING_AES_KEY'] = WECHAT_AES_KEY
-robot.config["APP_ID"] = WECHAT_APPID
-robot.config["APP_SECRET"] = WECHAT_APPSECRET
+miubot = WeRoBot()
+miubot.config['TOKEN'] = WECHAT2_TOKEN
+miubot.config['ENCODING_AES_KEY'] = WECHAT2_AES_KEY
+miubot.config["APP_ID"] = WECHAT2_APPID
+miubot.config["APP_SECRET"] = WECHAT2_APPSECRET
 
-media_ids = get_media_ids()
-poem_lines = get_poem_lines()
+media_ids = get_media_ids2()
+poem_lines = get_poem_lines2()
 
-@robot.filter(re.compile(WECHAT_CAT_WORDS))
+@miubot.filter(re.compile(WECHAT_CAT_WORDS))
 def handle_cat(message):
     logging.debug('handle_cat msg=%s' % message)
     if media_ids:
@@ -33,13 +33,13 @@ def handle_cat(message):
         return ImageReply(message, media_id=media_id)
     return "没找到猫图喵~~~"
 
-@robot.handler
+@miubot.handler
 def handle_all(message):
     if poem_lines:
         return random.choice(poem_lines)
     else:
         return "咕噜咕咕咕咕咕噜咕咕"
 
-@robot.error_page
+@miubot.error_page
 def make_error_page(url):
     return WECHAT_ERROR_HTML
--- utils.py
+++ utils.py
@@ -10,6 +10,14 @@ def get_media_ids():
     with open('media_ids.txt') as f:
         return f.read().splitlines()
 
+def get_media_ids2():
+    with open('media_ids2.txt') as f:
+        return f.read().splitlines()
+
 def get_poem_lines():
     with codecs.open('poems.txt', 'r', 'utf8') as f:
+        return f.read().splitlines()
+
+def get_poem_lines2():
+    with codecs.open('poems2.txt', 'r', 'utf8') as f:
         return f.read().splitlines()
